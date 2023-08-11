159dddf94a15e1b361db6839d52a315424de3b10
Frans <frans_421_12@hotmail.com>
2023-08-10 21:04:26 -0500

    feat: Api auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,43 +4,84 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+// use Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $request->validate([
-            'name' =>'required',
-            'email' =>'required|email|unique:users',
-            'password' => 'required|confirmed'
+        // $request->validate([
+        //     'name' =>'required',
+        //     'email' =>'required|email|unique:users',
+        //     'password' => 'required|confirmed'
+        // ]);
+        // $user = new User();
+        // $user->name = $request->name;
+        // $user->email = $request->email;
+        // $user->password = Hash::make($request->password);
+        // $user->save();
+        // return response($user, Response::HTTP_CREATED);
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string'
         ]);
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->save();
-        return response($user, Response::HTTP_CREATED);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(["data" => $user, "access_token" => $token, "token_type" => "Bearer", ]);
     }
 
     public function login(Request $request)
     {
+        // $credentials = $request->validate([
+        //     'email' => ['required', 'email'],
+        //     'password' => ['required']
+        // ]);
 
-    }
+        // if (Auth::attempt($request->only('email'), 'password')) {
+            // $user = Auth::user();
+            // $token = $user->createToken('token')->plainTextToken;
+            // $cookie = cookie('cookie_token', $token, 60 * 24);
+            // return response(["token" => $token], Response::HTTP_OK)->withoutCookie($cookie);
+        // } else {
+        //     return response(["message" => "Credenciales inválidas"], Response::HTTP_UNAUTHORIZED);
+        // }
 
-    public function userProfile(Request $request)
-    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(["message" => "Unauthorized", 401]);
+        }
+        
+        $user = User::where('email', $request['email'])->firstOrfail();
+        $token = $user->createToken('auth_token')->plainTextToken;
 
+        return response()->json([
+            "message" => "HI ".$user->name,
+            "accessToken" => $token,
+            "token_type" => "Bearer",
+            "user" => $user,
+        ]);
     }
 
     public function logout()
     {
+        auth()->user()->tokens()->delete();
 
-    }
-
-    public function allUser()
-    {
-
+        return [
+            "message" => "You have successfully logged out and the token was successfully deleted"
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::apiResource('services', ServiceController::class);
+});
+
 Route::apiResource('clients', ClientController::class);
-Route::apiResource('services', ServiceController::class);
 
 Route::post('clients/service', [ClientController::class, 'attach']);
 Route::post('services/service/detach', [ClientController::class, 'detach']);
