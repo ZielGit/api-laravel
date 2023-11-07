970b07380a3a76d218683dd75bb454faabebfefc
Frans <frans_421_12@hotmail.com>
2023-11-07 18:21:51 -0500

    fix: Role name

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,7 +13,7 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' => 'Administrator']);
+        Role::create(['name' => 'Administrador']);
         Role::create(['name' => 'Usuario']);
     }
 }
