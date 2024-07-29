dd71b771a2e9277ae894f1f97e0a83b2b32e293f
Frans <frans_421_12@hotmail.com>
2024-07-28 19:08:03 -0500

    feat: Invoice and relationship

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'document_type',
+        'document_number',
+        'name',
+        'last_name',
+        'email',
+        'phone',
+        'address'
+    ];
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
 
     public function services()
     {
